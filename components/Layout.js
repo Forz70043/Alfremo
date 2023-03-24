b33bff0b608d0072ec0d37283aee66cf37a18638
Forz70043 <alfonso.pisicchio@libero.it>
2023-03-24 18:39:48 +0100

    Update Layout with footer & without navbar

--- components/Layout.js
+++ components/Layout.js
@@ -1,16 +1,16 @@
 import Header from './Header'
-import NavbarComponent from './Navbar';
+import FooterComponent from './FooterComponent';
 
 export default function Layout({children}){
     
     return (
         <>
         <Header title="Alfremo"/>
-        <NavbarComponent currentPage={children.type.name.toLowerCase()} titleBrand="Alfremo" />
+        {/* {<NavbarComponent currentPage={children.type.name.toLowerCase()} titleBrand="Alfremo" />} */}
         <main>
             {children}
         </main>
-        
+        <FooterComponent />
         </>
     )
 }
