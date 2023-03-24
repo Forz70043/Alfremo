cf2533996f6fe78f4f5cbcf8715f984508e7218a
Forz70043 <alfonso.pisicchio@libero.it>
2023-03-24 18:40:26 +0100

    Added register page & comp.

---
+++ pages/register.js
@@ -0,0 +1,9 @@
+import RegisterComponent from "@/components/RegisterComponent"
+
+export default function Regsiter() {
+    return(
+        <>
+            <RegisterComponent registerTitle="Alfremo"/>
+        </>
+    )
+}
