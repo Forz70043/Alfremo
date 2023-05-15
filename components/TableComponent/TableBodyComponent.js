4f713f2975c90074a91f383d779167383dc8744f
Dantesk <lagalante.p@gmail.com>
2023-05-15 15:26:19 +0200

    fix width delete button

--- components/TableComponent/TableBodyComponent.js
+++ components/TableComponent/TableBodyComponent.js
@@ -57,7 +57,7 @@ export default function TableBodyComponent() {
                                 <Table.Cell>{user.lastName}</Table.Cell>
                                 <Table.Cell>
                                     <Link type="button" href={`/users/edit/${user.id}`} className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 me-1">Edit</Link>
-                                    <button onClick={() => deleteUser(user.id)} className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900" style={{ width: '60px' }} disabled={user.isDeleting}>
+                                    <button onClick={() => deleteUser(user.id)} className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900" style={{ width: '90px' }} disabled={user.isDeleting}>
                                         {user.isDeleting
                                             ? <span className="spinner-border spinner-border-sm"></span>
                                             : <span>Delete</span>
