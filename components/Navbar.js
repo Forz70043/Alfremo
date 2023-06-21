0dfc92f62ac36b2f9c38b1e8b1588694d8bf9d37
Forz70043 <alfonso.pisicchio@libero.it>
2023-06-21 12:56:35 +0200

    Update logo on navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -20,7 +20,7 @@ export default function NavbarComponent(props) {
         <>
             <Navbar fluid={true} rounded={false}>
                 <Navbar.Brand href="/">
-                    <img src="https://flowbite.com/docs/images/logo.svg" className="mr-3 h-6 sm:h-9" alt="Alfremo Logo" />
+                    <img src="/logo_alfremo.jpg" className="mr-3 h-6 sm:h-9" alt="Alfremo Logo" />
                     <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">{props.titleBrand ?? ''}</span>
                 </Navbar.Brand>
                 <Navbar.Toggle />
